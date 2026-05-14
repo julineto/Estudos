11620b1f9be6513c9fbbce73fec36f9b3de2472d
julineto <heyjuliocesar00@gmail.com>
2026-05-13 21:46:34 -0300

    Add files via upload

---
+++ Golang/varconst.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Variáveis
+	// var + nome da variável + tipo
+	//var nome string
+	//nome = "Júlio"
+	//fmt.Println(nome)
+
+	//nome = "César"
+	//fmt.Println(nome)
+
+	//var + idade + tipo
+	var idade int
+	idade = 25
+	fmt.Println(idade)
+
+	//var b, c int = 1, 2
+	//fmt.Println(b)
+	//fmt.Println(c)
+
+	//var d = true
+	//fmt.Println(d)
+
+	float := 3.14
+	fmt.Println(float)
+
+	//Constantes
+	const idadeJulio = 25
+	fmt.Println(idadeJulio)
+
+}

3d4227be9cfaacbcd4be30683df03d058494371e
julineto <heyjuliocesar00@gmail.com>
2026-05-13 21:44:18 -0300

    Maps

---
+++ Golang/maps.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// idade := map[string]int{}
+	// idade["Júlio"] = 25
+	// idade["César"] = 30
+	// fmt.Println(idade)
+	// fmt.Println(idade["Júlio"], idade["César"])
+
+	// anoNasc := map[string]int{
+	// 	"Júlio": 1999,
+	// 	"César": 2000,
+	// }
+	// fmt.Println(anoNasc)
+	// fmt.Println(anoNasc["Júlio"])
+	// fmt.Println(anoNasc["César"])
+
+	valorPrv := map[string]float64{
+		"valor": 1050.50,
+	}
+	fmt.Println(valorPrv)
+	fmt.Println(valorPrv["valor"])
+	anoPag := map[string]float64{
+		"Cogna": 20.26,
+	}
+	fmt.Println(anoPag)
+
+	// // maps [string] int
+	//{"Júlio": 25}
+	// map [string] string
+	//{"Júlio": "César"}
+
+}

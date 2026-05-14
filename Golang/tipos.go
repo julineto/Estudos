3f88cabace111eb9c5849e94d53e109bfd3c8c77
julineto <heyjuliocesar00@gmail.com>
2026-05-13 21:46:16 -0300

    Add files via upload

---
+++ Golang/tipos.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//bool (true/false)
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+
+	//string - sequência de bytes
+	//fmt.Printf("Type: %T - Value: %v\n", "Eu estou adorando Lorna Shore", "Eu estou adorando Lorna Shore")
+
+	//int - números inteiros
+	//fmt.Printf("Type: %T - Value: %v\n", 25, 25)
+
+	//float *(float64/float32) - decimal
+	//fmt.Printf("Type: %T - Value: %v\n", 25.25, 25.25)
+
+}
